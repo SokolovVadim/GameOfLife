8628a959bacda7e45c8a069a9b4a79efbe9fc0b2
Vadim <varem99@mail.ru>
2020-07-15 00:18:28 +0300

    topology added

--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,47 @@
 #include <SFML/Graphics.hpp>
 #include "engine.hpp"
+
+enum STATES
+{
+    ALIVE = 0,
+    DEAD = 1
+};
+
+class Cell
+{
+    STATES status;
+    // image, texture, sprite
+};
+
+void init()
+{
+    
+}
+
+void update()
+{
+
+}
+
+void draw()
+{
+    
+}
+
+void snd_data()
+{
+
+}
+
+void recv_data()
+{
+
+}
+
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "Game of Life");
+    sf::RenderWindow window(sf::VideoMode(512, 512), "Game of Life");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -16,6 +55,8 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+
 
         window.clear();
         window.draw(shape);
